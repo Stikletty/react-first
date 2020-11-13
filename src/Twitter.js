27772c27ece432f5baf8cea3e9e52474dd70db73
Gabor Troll <gabor.troll@gekko-marketing.com>
2020-11-13 17:39:20 +0100

    first try

---
+++ src/Twitter.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Tweet.css';
+
+function Twitter() {
+  return (
+    <div className="tweet">
+      <h2>Header</h2>
+      <p>Szöveg</p>
+      <h2>Retweets</h2>
+    </div>
+  );
+}
+
+export default Twitter;
